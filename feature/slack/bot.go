121ee6dced2dbdf073c77b709e80a22e19b523e0
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2025-01-08 23:50:32 +0900

    bot 을 실행하는 로직 추가

--- feature/slack/bot.go
+++ feature/slack/bot.go
@@ -1,5 +1,29 @@
 package slack
 
+import (
+	"log"
+	"os"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/socketmode"
+)
+
 func Bot() {
 
+	token := os.Getenv("SLACK_TOKEN")
+	appToken := os.Getenv("APP_TOKEN")
+
+	client := slack.New(token, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
+
+	app := socketmode.New(
+		client,
+		socketmode.OptionDebug(true),
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	err := app.Run()
+	if err != nil {
+		log.Fatalf("Error starting socketmode listener: %v", err)
+		return
+	}
 }
