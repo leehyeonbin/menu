2b5b334c1f7ff8fa687b4c2f89fefbafcfdbb368
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2025-01-12 15:50:07 +0900

    print 추가

--- util/downloadimage.go
+++ util/downloadimage.go
@@ -7,19 +7,29 @@ import (
 	"os"
 )
 
-// 이미지 다운로드 함수
+// DownloadImage 이미지 다운로드 함수
 func DownloadImage(url, filename string) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		return fmt.Errorf("failed to download image: %w", err)
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Errorf("failed to render image: %w", err)
+		}
+	}(resp.Body)
 
 	file, err := os.Create(filename)
 	if err != nil {
 		return fmt.Errorf("failed to create file: %w", err)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Errorf("failed to create file: %w", err)
+		}
+	}(file)
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
